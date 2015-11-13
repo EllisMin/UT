d6d01f6998be627a64162973e9d7ea57576865fd
EllisMin <dbmin@hotmail.com>
2015-11-13 15:32:02 -0600

    added a little more view

--- login/user.swift
+++ login/user.swift
@@ -12,30 +12,31 @@ import ParseUI
 
 class user: UIViewController{
     @IBAction func logoutPressed(sender: AnyObject) {
-               PFUser.logOut()
-
+        PFUser.logOut()
+        tabBarController?.selectedIndex = 0 // open the first tab bar
+        
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
+    
     /*
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
     }
     */
-
+    
 }
